c186d71920045f222b4031506fa1e4061b1f4ca3
Adrian-Tudor Panescu <adrian@panescu.com>
2015-12-09 17:16:28 +0200

    www: user data endpoint

--- src/www/js/ui.js
+++ src/www/js/ui.js
@@ -1,1 +1,14 @@
-console.log(window.bag);
+window.fetch('api/get', {
+    'method': 'post',
+    'headers': {
+        'Content-Type': 'application/json'
+    },
+    'body': JSON.stringify({
+        'accessToken': window.bag.accessToken
+    })
+})
+.then((data) => {
+    return data.json();
+})
+.then(console.log.bind(console))
+.catch(console.log.bind(console));
